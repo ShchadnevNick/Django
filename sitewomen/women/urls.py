c5c19331237bf98ab5e59d270f214b8b6bd4a5ce
Nicolay <nicolay-shchadnev@mail.ru>
2024-12-30 00:35:22 +0500

    Created a converter

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-from women.views import index, about
+from django.urls import path, re_path, register_converter
+
+from women.converters import FourDigitYeadConverter
+from women.views import index, about, categories, archive
+
+register_converter(FourDigitYeadConverter, 'year4')
+
 
 urlpatterns = [
     path('', index),
     path('about/', about),
+    path('cats/<slug:cat_slug>/', categories),
+    path('archive/<year4:year>/', archive)
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 def index(request):
     return HttpResponse('<h1>Главная страница!</h1>')
@@ -6,3 +6,10 @@ def index(request):
 def about(request):
     return HttpResponse('<h1>О сайте</h1>')
 
+def categories(request, cat_slug):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f'<h1>Страница с категории {cat_slug}')
+
+def archive(request, year):
+    return HttpResponse(f'Страница с архивом {year} года')
