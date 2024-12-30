943797c3691f704fb6d5bb1121588d4a95338113
Nicolay <nicolay-shchadnev@mail.ru>
2024-12-30 20:14:07 +0500

    READY Templates, render, {{par.a|filter:''}}, tags - {{% if, for, forloop.last %}}

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,12 +1,26 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponsePermanentRedirect
-from django.shortcuts import redirect
+from django.shortcuts import render
 from django.urls import reverse
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
+
+
 def index(request):
-    return HttpResponse('<h1>Главная страница!</h1>')
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
+    }
+    return render(request, 'women/index.html', data)
 
 def about(request):
-    return HttpResponse('<h1>О сайте</h1>')
+    data = {'title': 'О сайте'}
+    return render(request, 'women/about.html', data)
 
 def categories(request, cat_slug):
     return HttpResponse(f'<h1>Страница с категорией {cat_slug}')
